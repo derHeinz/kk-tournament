2671bbee8a6a2833b3728b7bf079a3462b3c9ee7
4otkiymaster <ruka_b@ukr.net>
2024-03-07 11:46:14 +0100

    WIP

--- src/main/java/com/github/chotkiymaster/Field.java
+++ src/main/java/com/github/chotkiymaster/Field.java
@@ -3,11 +3,15 @@ package com.github.chotkiymaster;
 import java.util.List;
 
 public class Field {
-    private List<Square> squares;
+    private Square[][] squares;
 
     public Field(int countX, int countY) {
         //TODO
+        this.squares = new Square[countX][countY];
         Wall[] walls = new Wall[]{new Wall(), new Wall(), new Wall(), new Wall()};
         Square square1 = new Square(walls[0], walls[1], walls[2], walls[3]);
+        Square square2 = new Square();
+        square2.setLeftWall(walls[0]);
+        
     }
 }
--- src/main/java/com/github/chotkiymaster/Square.java
+++ src/main/java/com/github/chotkiymaster/Square.java
@@ -2,6 +2,38 @@ package com.github.chotkiymaster;
 
 public class Square {
     private Wall upperWall;
+    public Wall getUpperWall() {
+        return upperWall;
+    }
+
+    public void setUpperWall(Wall upperWall) {
+        this.upperWall = upperWall;
+    }
+
+    public Wall getLeftWall() {
+        return leftWall;
+    }
+
+    public void setLeftWall(Wall leftWall) {
+        this.leftWall = leftWall;
+    }
+
+    public Wall getRightWall() {
+        return rightWall;
+    }
+
+    public void setRightWall(Wall rightWall) {
+        this.rightWall = rightWall;
+    }
+
+    public Wall getBottomWall() {
+        return bottomWall;
+    }
+
+    public void setBottomWall(Wall bottomWall) {
+        this.bottomWall = bottomWall;
+    }
+
     private Wall leftWall;
     private Wall rightWall;
     private Wall bottomWall;
@@ -11,5 +43,8 @@ public class Square {
         this.leftWall = leftWall;
         this.rightWall = rightWall;
         this.bottomWall = bottomWall;
+    }
+    public Square(){
+        //Bewusst: nichts
     }
 }
